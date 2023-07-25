6d7f47d3120e24b555c8102f26c994fa0539df15
Ubaid196 <Ubaid196>
2023-07-25 15:55:26 +0500

    updated

--- src/components/views/AllProducts/index.tsx
+++ src/components/views/AllProducts/index.tsx
@@ -1,19 +1,20 @@
-"use client"
+"use client";
 import { oneProductType } from "@/components/utils/ProductsDataArrayAndTypes";
 import { Component, ReactNode } from "react";
 
-interface propsType{
-    productArray:Array<oneProductType>
+interface propsType {
+  productArray: Array<oneProductType>;
 }
 
-export default class AllProductsComponent extends Component<{productData:propsType}> {
-    getData = ()=>{
-        console.log(this.props.productData.productArray[0].price);
-        
-    }
-    render():ReactNode{
-        return(
-            <div onClick={this.getData}>{this.props.productData.productArray[0].price}</div>
-        )
-    }
+export default class AllProductsComponent extends Component<{
+  productData: propsType;
+}> {
+  start: number = 10;
+  getData = () => {
+    console.log(this.props.productData.productArray[0].price);
+    console.log(this.props.productData);
+  };
+  render(): ReactNode {
+    return <div onClick={this.getData}>HI</div>;
+  }
 }
