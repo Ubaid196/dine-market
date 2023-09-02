0ecc42ccf2a41ea91df57dd41d6079d393f828fc
Ubaid196 <Ubaid196>
2023-09-02 17:28:07 +0500

    resolved some errors

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,14 +3,14 @@ import ContextWrapper from "@/global/Context";
 
 
 
-export async function fetchAllStoreProducts() {
+async function fetchAllStoreProducts() {
   let res = await fetch(
     `https://eki3q64k.api.sanity.io/v2023-07-02/data/query/production?query=*[_type == 'products']`,{
      cache: "no-store"
     }
   )
 
-  return res.json()
+  return res.json() 
 }
 
 
@@ -19,9 +19,9 @@ const Cart = async() => {
   let allProductsOfStore = await fetchAllStoreProducts()
 
   return (
-    
+    <ContextWrapper>
     <CartComp allProductsOfStore={allProductsOfStore.result}/>
-    
+    </ContextWrapper>
   )
 };
 
